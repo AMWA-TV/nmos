01a12eee757b8101917de469359d08be7d0084b0
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-01-26 15:31:29 +0000

    Removed old .init-scripts

--- .init-scripts/make-build-tools.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-git clone --single-branch --branch external-site https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
-.scripts/install-dependencies.sh
--- .init-scripts/make-distclean.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-set -o errexit
-
-rm -rf source-repo branches tags index.md index-contents.md
-rm -rf node_modules/ yarn.lock package-lock.json .scripts/ .layouts/ _layouts/ assets/ raml2html-nmos-theme/ _site/
