d542a44682215d932394e0b0bcf58f74e0c70e4f
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-02-15 18:20:05 +0000

    Use main branch of nmos-lint-scripts

--- .lint/.init-scripts/make-build-tools.sh
+++ .lint/.init-scripts/make-build-tools.sh
@@ -2,5 +2,5 @@
 
 set -o errexit
 
-git clone --single-branch --branch use-lint-dir https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-lint-scripts .scripts
+git clone --single-branch --branch main https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-lint-scripts .scripts
 .scripts/install-dependencies.sh
