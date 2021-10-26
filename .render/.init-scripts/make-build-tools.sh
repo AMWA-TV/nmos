cd7e645efa2eb7a53ce935915b42696330cf63ed
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-10-26 22:02:10 +0100

    Use main branch for build scripts

--- .render/.init-scripts/make-build-tools.sh
+++ .render/.init-scripts/make-build-tools.sh
@@ -2,5 +2,4 @@
 
 set -o errexit
 
-git clone --single-branch --branch deploy-action https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
-.scripts/install-dependencies.sh
+git clone --single-branch --branch "${NMOS_DOC_BUILD_SCRIPTS_BRANCH:-main}" https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
