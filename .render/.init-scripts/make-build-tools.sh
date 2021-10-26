8bbe45124e6dcd4bf84218c5bf3e76008c2cd812
Peter Brightwell <peter.brightwell@bbc.co.uk>
2021-10-26 18:43:41 +0100

    Fix branch typo

--- .render/.init-scripts/make-build-tools.sh
+++ .render/.init-scripts/make-build-tools.sh
@@ -2,5 +2,5 @@
 
 set -o errexit
 
-git clone --single-branch --branch deploy-action "${NMOS_DOC_BUILD_SCRIPTS_BRANCH:-main}" https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
+git clone --single-branch --branch deploy-action https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
 .scripts/install-dependencies.sh
