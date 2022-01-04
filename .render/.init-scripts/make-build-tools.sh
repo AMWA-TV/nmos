e0f84c9ffdfdef503a7eb5910c3f705072b96403
Peter Brightwell <peter.brightwell@bbc.co.uk>
2022-01-04 11:51:15 +0000

    Don't run make-build-tools.sh from itself!

--- .render/.init-scripts/make-build-tools.sh
+++ .render/.init-scripts/make-build-tools.sh
@@ -3,4 +3,3 @@
 set -o errexit
 
 git clone --single-branch --branch "${NMOS_DOC_BUILD_SCRIPTS_BRANCH:-main}" https://${GITHUB_TOKEN:+${GITHUB_TOKEN}@}github.com/AMWA-TV/nmos-doc-build-scripts .scripts
-.init-scripts/make-build-tools.sh
